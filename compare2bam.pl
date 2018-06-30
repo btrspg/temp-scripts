8e785468350fc5d27d7f0464d8c5bf88c1e541cc
btrspg <yuelong_chen@yahoo.com>
2018-06-30 10:23:24 +0800

    bam比较修改

--- compare2bam.pl
+++ compare2bam.pl
@@ -16,36 +16,53 @@ my $total=0;
 my $same = 0;
 while(my $line1=<IN1> and my $line2=<IN2>){
 	$total++;
+	$line1 =~ s/RG:Z:.+//;
+	$line2 =~ s/RG:Z:.+//;
 	if ($line1 eq $line2){
 		$same++;
 	}
 	else{
-		print "------------------------\n";
-		print "line1:$line1\n";
-		print "line2:$line2\n";
-#		print "+++++++++++++++++++++++++++++\n";
+		#		print "------------------------\n";
+		#		print "line1:$line1\n";
+		#		print "line2:$line2\n";
+		#		print "+++++++++++++++++++++++++++++\n";
 		my @cell1s = split /\t/,$line1;
 		my @cell2s = split /\t/,$line2;
-		for(my $i=0;$i<@cell1s;$i++){
+		#		next if $cell1s[0] eq $cell2s[0];
+		#		print "$total\n";
+		my $flag = 1;
+		my $str1;
+		my $str2;
+		#		next if abs($cell1s[1]-$cell2s[1]) >2;
+		for(my $i=0;$i<@cell1s-1;$i++){
 			if($cell1s[$i] ne $cell2s[$i]){
 				my @e1s = split //,$cell1s[$i];
 				my @e2s = split //,$cell2s[$i];
-				my $str1;
-				my $str2;
 				for(my $j=0;$j<@e1s;$j++){
 					if($e1s[$j] eq $e2s[$j]){
 						$str1="$str1$e1s[$j]";
 						$str2="$str2$e2s[$j]";
 					}
 					else{
+						$flag = 0;
 						$str1="$str1\[$e1s[$j]\]";
 						$str2="$str2\[$e2s[$j]\]";
 					}
 				}
-				print "1:$str1\n";
-				print "2:$str2\n";
 			}
 		}
+		if($flag == 0){
+			print "----------------------\n";
+			print "line1:$line1\n";
+			print "line2:$line2\n";
+			print "1:$str1\n";
+			print "2:$str2\n";
+		}
+		else{
+			$same++;
+		}
+			
+		
 #		sleep(5);
 	}
 
