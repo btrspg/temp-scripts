5a6d67323a0853b288c32f9d917e1dcbf3fefd53
btrspg <yuelong_chen@yahoo.com>
2018-07-01 22:22:44 +0800

    输出bed

---
+++ filter_low_depth.pl
@@ -0,0 +1,50 @@
+use strict;
+
+die "perl $0 <depth.gz> <filter1,filter2,> <prefix>" unless @ARGV == 3;
+
+my $depth = shift;
+my $filter = shift;
+my $outdir = shift;
+
+my @fts = split /,/,$filter;
+
+my %hash;
+my %locs;
+
+open IN,"less $depth| " || die $!;
+while(<IN>){
+	s/[\r\n]+//;
+	my @cells = split /\t/;
+	foreach my $ft(@fts){
+		if($cells[2]<$ft){
+			$hash{$ft}++;
+			$locs{$ft}{$cells[0]}{$cells[1]}=1;
+		}
+	}
+}
+close IN;
+
+foreach my $key(keys %hash){
+	open OUT,">$outdir\_$key.stats" || die $!;
+	print "total: $hash{$key} site below $key\n";
+	foreach my $v(keys %{$locs{$key}}){
+		my $last=0;
+		my $start=0;
+		foreach my $loca(sort {$a<=>$b} keys %{$loc{$key}{$v}}){
+			if ($loca == $last+1){
+				$last = $loca;
+			}
+			elsif($last==0){
+				$start=$loca;
+				$last = $loca;
+			}
+			else{
+				print OUT "$V\t$start\t$last\n";
+				$start = $loca;
+				$end = $loca;
+			}
+		}
+#		print OUT "$tmps[0]\t$tmps[1]\t".($tmps[1]+1)."\n";
+	}
+	close OUT
+}
