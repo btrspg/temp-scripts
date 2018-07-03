c59acac7c4392022cae23ea297e3a3459e1fc7c5
btrspg <yuelong_chen@yahoo.com>
2018-07-03 18:33:38 +0800

    fix bug

--- control_plot.pl
+++ control_plot.pl
@@ -1,5 +1,5 @@
 use strict;
-
+use File::Basename;
 my $python='/home/chenyuelong/venv/py_cosmos2_pipeline/bin/python';
 my $plotpy='/home/chenyuelong/pipeline/FH_analysis_pipeline/src/plotbed.py';
 
@@ -13,7 +13,7 @@ system("mkdir -p $outdir");
 
 my @depths = glob("$pattern");
 foreach my $depth(@depths){
-	my $name = (split /_/,basename($depth))[0];
+	my $name = (split /\./,basename($depth))[0];
 	my $cmd = "$python $plotpy --depth $depth --depthout $outdir/$name.png --bed $bed";
 	system("$cmd");
 }
