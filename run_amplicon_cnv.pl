e8458a0843b6d7525024306c8d3b07d024512ed2
btrspg <yuelong_chen@yahoo.com>
2018-07-04 18:14:53 +0800

    update some tips

--- run_amplicon_cnv.pl
+++ run_amplicon_cnv.pl
@@ -1,23 +1,40 @@
 use strict;
 use File::Basename;
 
-die "perl $0 <pattern> o<outdir> <python> <bed-gc>" unless @ARGV==4;
+die "perl $0 <pattern> <outdir> <python> <bed-gc> <controls>" unless @ARGV==5;
 
 my $pattern =shift;
 my $outdir = shift;
 my $python = shift;
 my $bed = shift;
+my $cf = shift;
 system("mkdir -p $outdir");
 
 my @files = glob("$pattern");
 
-my $controls = join(" ",@files);
+my @tmps;
+my $dir = dirname($pattern);
+open IN,$cf || die $!;
+while(<IN>){
+	s/[\r\n]+//;
+	#print "$_\n";
+	#my $test = "$dir/$_";
+	#	print "test:$test\n";
+	my @tfs = glob("$dir/$_");
+	#	print "@tfs\n";
+	push(@tmps,@tfs);
+}
+close IN;
+my $controls = join(" ",@tmps);
+#print "$controls\n";
+#my $controls = join(" ",@files);
 my $n=0;
 foreach my $file(@files){
 	my $name = (split /\./,basename($file))[0];
 	my $cmd = "$python ~/pipeline/amplicon-cnv/amplicon-cnv.py --case $file --controls $controls --bed-gc $bed --output $outdir/$name.cnv.txt";
 	$n++;
 	print "$n\n";
-	#print "$cmd\n";
+	#	print "$cmd\n";
+	#last;
 	system($cmd);
 }
--- temp-se-pipeline.pl
+++ temp-se-pipeline.pl
@@ -1,40 +1,41 @@
 use strict;
 use File::Basename;
 
-die "perl $0 <pattern> <outdir> " unless @ARGV==2;
+die "perl $0 <pattern> <outdir> <sh>" unless @ARGV==3;
 
 my $pattern = shift;
 my $outdir = shift;
-
+my $sh = shift;
 
 my @files = glob("$pattern");
+open SH,">$sh" || die $!;
 foreach my $file(@files){
 	my $name = (split /\./,basename($file))[0];
 	if($file =~ /\.fq$/){
 		my $cmd = "make -f /wes/chenyl/projects/rd_template_pipeline/makefile CONFIG=/wes/chenyl/projects/rd_template_pipeline/config sample=$name outdir=$outdir fq1=$file  ALL -j2";
 		print "-------------------\n";
-		print "$cmd\n";
+		print SH "$cmd\n";
 		system("$cmd");
 	}
 	elsif($file =~ /\.fq\.bz2/){
 
 		my $cmd1 = "bunzip2 $file";
-		$file =~ s/\.fq.bz2//;
+		$file =~ s/\.bz2//;
 		my $cmd2 = "make -f /wes/chenyl/projects/rd_template_pipeline/makefile CONFIG=/wes/chenyl/projects/rd_template_pipeline/config sample=$name outdir=$outdir fq1=$file  ALL -j2";
 		print "--------------------\n";
 		print "$cmd1\n";
 		system($cmd1);
-		print "$cmd2\n";
+		print SH "$cmd2\n";
 		system($cmd2);
 	}	
 	elsif($file =~ /\.fq\.tar\.bz2/){
 		my $cmd1 = "tar -jxvf $file";
-		$file =~ s/\.fq\.tar\.bz2//;
+		$file =~ s/\.tar\.bz2//;
 		my $cmd2 = "make -f /wes/chenyl/projects/rd_template_pipeline/makefile CONFIG=/wes/chenyl/projects/rd_template_pipeline/config sample=$name outdir=$outdir fq1=$file  ALL -j2";
 		print "-------------------\n";
 		print "$cmd1\n";
 		system($cmd1);
-		print "$cmd2\n";
+		print SH "$cmd2\n";
 		system($cmd2);
 	}
 }
