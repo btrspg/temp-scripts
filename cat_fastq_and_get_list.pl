7dce039cee22540451bc386166868dedf25d0ae7
btrspg <yuelong_chen@yahoo.com>
2018-07-11 11:41:46 +0800

    get_list修改的不仅识别fq.gz,还识别fastq.gz

--- cat_fastq_and_get_list.pl
+++ cat_fastq_and_get_list.pl
@@ -17,7 +17,7 @@ my @files = glob("$pattern");
 my %name_hash;
 foreach my $file(@files){
 	my $name = (split /_/,basename($file))[0];
-	$name_hash{$name}=dirname($file)
+	$name_hash{$name}=dirname($file);
 }
 
 system("mkdir -p $outdir");
@@ -25,9 +25,9 @@ system("mkdir -p $outdir");
 open SH,">$outdir/cat.sh" || die $!;
 open LIST,">$list" || die $!;
 
-foreach my $nam(keys %name_hash){
-	my @read1s = glob("$name_hash{$nam}/$nam*_1.fq.gz");
-	my @read2s = glob("$name_hash{$nam}/$nam*_2.fq.gz");
+foreach my $nam(sort keys %name_hash){
+	my @read1s = glob("$name_hash{$nam}/$nam/clean_data/*_1.fq.gz");
+	my @read2s = glob("$name_hash{$nam}/$nam/clean_data/*_2.fq.gz");
 	my $con = "$nam";
 	if (@read1s > 0){
 		print SH "cat @read1s > $outdir/$nam.R1.fq.gz\n";
--- get_list.pl
+++ get_list.pl
@@ -6,9 +6,15 @@ die "perl $0 <dir>" unless @ARGV==1;
 my $dir = shift;
 my @gzs = glob("$dir/*1.fq.gz");
 
+my $tag = 'fq.gz';
+if (@gzs ==0){
+	@gzs=glob("$dir/*1.fastq.gz");
+	$tag='fastq.gz';
+}
+
 foreach my $gz(@gzs){
 	my $name = (split /_/,basename($gz))[0];
 	my $fq2 = $gz;
-	$fq2 =~ s/_1\.fq\.gz/_2.fq.gz/;
+	$fq2 =~ s/1\.$tag/2\.$tag/;
 	print "$name\t$gz\t$fq2\n";
 }
